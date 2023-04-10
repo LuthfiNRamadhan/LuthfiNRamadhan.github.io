3ad29abf378435faa72772263fc49f8eab8191e7
LuthfiNRamadhan <strinstring183@gmail.com>
2023-04-10 11:49:43 +0700

    Add some file Portofolio

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Parallax - Portofolio
+    $('.jumbotron h2').css({
+        'transform' : 'translate(0px, '+ wScroll/2.1 +'%)'
+    });
+    
+    $('.jumbotron center p').css({
+        'transform' : 'translate(0px, '+ wScroll/1.7 +'%)'
+    });
+
+    $('.jumbotron center a').css({
+        'transform' : 'translate(0px, '+ wScroll/1.7 +'%)'
+    });
+
+// Parallax - Portofolio
+    $('.portofolio row col h3').css({
+        'transform' : 'translate(0px, '+ wScroll/2.1 +'%)'
+    });
+    
+    $('.portofolio row col p').css({
+        'transform' : 'translate(0px, '+ wScroll/1.7 +'%)'
+    });
+
+    $('.portofolio row col a').css({
+        'transform' : 'translate(0px, '+ wScroll/1.7 +'%)'
+    });
+
+    $('.portofolio row col img').css({
+        'transform' : 'translate(0px, '+ wScroll/1.7 +'%)'
+    });
+
+    
+    
